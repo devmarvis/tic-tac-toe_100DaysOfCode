9ded3e2909a621a36227bc790f41777302049fc9
devmarvis <erobagacollins@gmail.com>
2024-09-05 09:47:23 -0500

    fix spot already filled, replay functionality

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ n = 0
 while n < 9:
     game.print_board()
     if not game.replay:
-        print('Player X')
-        user_X = int(input('Mark where?\n>> ')) - 1
-        if game.check_winner(user_X, 'X'):
-            break
-        elif game.replay:
-            continue
-
-        print('Player O')
-        user_O = int(input('Mark where?\n>> ')) - 1
-        if game.check_winner(user_O, 'O'):
-            break
-        elif game.replay:
-            continue
+        if game.to_play:
+            print(f'Player {game.to_play}')
+            user_ = int(input('Mark where?\n>> ')) - 1
+            if game.check_winner(user_, game.to_play):
+                break
+            elif game.replay:
+                continue
+        else:
+            print('Player X')
+            user_X = int(input('Mark where?\n>> ')) - 1
+            if game.check_winner(user_X, 'X'):
+                break
+            elif game.replay:
+                continue
+            #
+            # print('Player O')
+            # user_O = int(input('Mark where?\n>> ')) - 1
+            # if game.check_winner(user_O, 'O'):
+            #     break
+            # elif game.replay:
+            #     continue
     else:
         print(f'Player {game.replay}')
         user = int(input('Mark where?\n>> ')) - 1
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 
+players = ['X', 'O']
 
 class Game:
     def __init__(self):
         self.board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
         self.replay = ''
+        self.to_play = ''
 
     def print_board(self):
         for i in range(0, 9, 3):
@@ -13,46 +15,47 @@ class Game:
 
     def check_winner(self, user, symbl):
         if self.board[user] != ' ':
+            self.replay = symbl
             self.print_board()
             print('Space already filled, Try again!')
-            self.replay = symbl
 
         else:
             self.board[user] = symbl
-            self.print_board()
             self.replay = ''
+            self.to_play = players[players.index(symbl) - 1]
+            self.print_board()
 
-        for i in range(0, len(self.board), 3):
-            mid_col = 1
-            r_dia = 2
-            if self.board[i] != ' ' and self.board[i] == self.board[i+1] == self.board[i+2]:
-                self.print_board()
-                print(f'Winner is Player {self.board[i]}')
-                return True
-
-            if i + 1 == mid_col:
-                if self.board[mid_col] != ' ' and self.board[mid_col] == self.board[mid_col+3] == self.board[mid_col+6]:
-                    self.print_board()
-                    print(f'Winner is Player {self.board[i + 1]}')
-                    return True
-
-            if i % 3 == 0 and i < 3:
-                if self.board[i] != ' ' and self.board[i] == self.board[i + 4] == self.board[i + 8]:
-                    self.print_board()
-                    print(f'Winner is Player {self.board[i]}')
-                    return True
-                elif self.board[i] != ' ' and self.board[i] == self.board[i + 3] == self.board[i + 6]:
+            for i in range(0, len(self.board), 3):
+                mid_col = 1
+                r_dia = 2
+                if self.board[i] != ' ' and self.board[i] == self.board[i+1] == self.board[i+2]:
                     self.print_board()
                     print(f'Winner is Player {self.board[i]}')
                     return True
 
-            if i % 3 == 0 and i - 1 == r_dia:
-                if self.board[r_dia] != ' ' and self.board[r_dia] == self.board[r_dia + 2] == self.board[r_dia + 4]:
-                    self.print_board()
-                    print(f'Winner is Player {self.board[i - 1]}')
-                    return True
-                elif self.board[r_dia] != ' ' and self.board[r_dia] == self.board[r_dia + 3] == self.board[r_dia + 6]:
-                    self.print_board()
-                    print(f'Winner is Player {self.board[i - 1]}')
-                    return True
+                if i + 1 == mid_col:
+                    if self.board[mid_col] != ' ' and self.board[mid_col] == self.board[mid_col+3] == self.board[mid_col+6]:
+                        self.print_board()
+                        print(f'Winner is Player {self.board[i + 1]}')
+                        return True
+
+                if i % 3 == 0 and i < 3:
+                    if self.board[i] != ' ' and self.board[i] == self.board[i + 4] == self.board[i + 8]:
+                        self.print_board()
+                        print(f'Winner is Player {self.board[i]}')
+                        return True
+                    elif self.board[i] != ' ' and self.board[i] == self.board[i + 3] == self.board[i + 6]:
+                        self.print_board()
+                        print(f'Winner is Player {self.board[i]}')
+                        return True
+
+                if i % 3 == 0 and i - 1 == r_dia:
+                    if self.board[r_dia] != ' ' and self.board[r_dia] == self.board[r_dia + 2] == self.board[r_dia + 4]:
+                        self.print_board()
+                        print(f'Winner is Player {self.board[i - 1]}')
+                        return True
+                    elif self.board[r_dia] != ' ' and self.board[r_dia] == self.board[r_dia + 3] == self.board[r_dia + 6]:
+                        self.print_board()
+                        print(f'Winner is Player {self.board[i - 1]}')
+                        return True
 
